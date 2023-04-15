152dd722ebe2dba4e91e72134a81190ca8ec354f
bed-com-37-20 <bed-com-37-20@unima.ac.mw>
2023-04-15 13:03:32 +0100

    i have created three classes and one main class for testing the other classes

---
+++ src/Employment.java
@@ -0,0 +1,14 @@
+import sun.awt.windows.WPrinterJob;
+
+import java.sql.SQLOutput;
+
+public class Employment extends Person {
+    String officeName;
+    private double salary;
+    private String date;
+
+    public Employment(){
+        System.out.println("last to excute");
+    };
+
+}
---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+   private String name;
+   private String address;
+   private String phonenumber;
+   private String e_mailAdress;
+   public Person(){
+      System.out.println("Hello world");
+   };
+
+
+}
